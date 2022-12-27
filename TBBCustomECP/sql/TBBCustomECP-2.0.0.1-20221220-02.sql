6e6345e88427ba179d4aa8317db14e4a781ff586
EmilyTsaiChien <tsaichieneen@gmail.com>
2022-12-27 11:32:13 +0800

    修改更版sql

--- TBBCustomECP/sql/TBBCustomECP-2.0.0.1-20221220-02.sql
+++ TBBCustomECP/sql/TBBCustomECP-2.0.0.1-20221220-02.sql
@@ -1,4 +1,7 @@
---電文------------------------------------------------------------------------------------------------------
+--電文--
+--R006------------------------------------------------------------------------------------------------------
+
+Go
 INSERT [dbo].[TpCUSmCsrTemplate] ([FId], [FName], [FIndex], [U_UpTemplate], [U_DownTemplate1], [U_DownTemplate2], [U_DownTemplate3], [FDescription], [U_ConnectionId], [U_DataModelUp], [U_DataModel], [FDataModel]) VALUES (N'c7bfb856-5000-8c56-0401-184e119f6e40', N'R006', 83, N'{
    "name":"R006",
    "from":"csr",
@@ -8,7 +11,7 @@ INSERT [dbo].[TpCUSmCsrTemplate] ([FId], [FName], [FIndex], [U_UpTemplate], [U_D
       "CUSIDN": ""
    }
 }', N'', N'', N'', N'信用卡客戶促銷活動明細檔查詢', N'ec9b8729-0c00-a162-6507-1798887712b0', N'Json', N'Json', NULL)
-
+--R007A
 INSERT [dbo].[TpCUSmCsrTemplate] ([FId], [FName], [FIndex], [U_UpTemplate], [U_DownTemplate1], [U_DownTemplate2], [U_DownTemplate3], [FDescription], [U_ConnectionId], [U_DataModelUp], [U_DataModel], [FDataModel]) VALUES (N'c7bfb856-5000-8c56-0401-184e11a60a30', N'R007A', 84, N'{
    "name":"R007A",
    "from":"csr",
@@ -18,7 +21,7 @@ INSERT [dbo].[TpCUSmCsrTemplate] ([FId], [FName], [FIndex], [U_UpTemplate], [U_D
       "CUSIDN": ""
    }
 }', N'', N'', N'', N'信用卡客戶約定明細檔查詢', N'ec9b8729-0c00-a162-6507-1798887712b0', N'Json', N'Json', NULL)
-
+--R007B
 INSERT [dbo].[TpCUSmCsrTemplate] ([FId], [FName], [FIndex], [U_UpTemplate], [U_DownTemplate1], [U_DownTemplate2], [U_DownTemplate3], [FDescription], [U_ConnectionId], [U_DataModelUp], [U_DataModel], [FDataModel]) VALUES (N'c7bfb856-5000-8c56-0401-184e11d3a470', N'R007B', 85, N'{
    "name":"R007B",
    "from":"csr",
@@ -30,7 +33,7 @@ INSERT [dbo].[TpCUSmCsrTemplate] ([FId], [FName], [FIndex], [U_UpTemplate], [U_D
       "CUSNUM":""
    }
 }', N'', N'', N'', N'信用卡客戶約定申請歷史檔查詢', N'ec9b8729-0c00-a162-6507-1798887712b0', N'Json', N'Json', NULL)
-
+--R008
 INSERT [dbo].[TpCUSmCsrTemplate] ([FId], [FName], [FIndex], [U_UpTemplate], [U_DownTemplate1], [U_DownTemplate2], [U_DownTemplate3], [FDescription], [U_ConnectionId], [U_DataModelUp], [U_DataModel], [FDataModel]) VALUES (N'c7bfb856-5000-8c56-0401-184e139b8990', N'R008', 86, N'{
    "name":"R008",
    "from":"csr",
@@ -40,7 +43,7 @@ INSERT [dbo].[TpCUSmCsrTemplate] ([FId], [FName], [FIndex], [U_UpTemplate], [U_D
       "CARDNUM": ""
    }
 }', N'', N'', N'', N'信用卡委繳類別檔查詢', N'ec9b8729-0c00-a162-6507-1798887712b0', N'Json', N'Json', NULL)
-
+--R021
 INSERT [dbo].[TpCUSmCsrTemplate] ([FId], [FName], [FIndex], [U_UpTemplate], [U_DownTemplate1], [U_DownTemplate2], [U_DownTemplate3], [FDescription], [U_ConnectionId], [U_DataModelUp], [U_DataModel], [FDataModel]) VALUES (N'c7bfb856-5000-8c56-0401-184e13b0ff60', N'R021', 87, N'{
    "name":"R021",
    "from":"csr",
@@ -50,7 +53,7 @@ INSERT [dbo].[TpCUSmCsrTemplate] ([FId], [FName], [FIndex], [U_UpTemplate], [U_D
       "SVCSCRD": ""
    }
 }', N'', N'', N'', N'悠遊卡資料檔查詢', N'ec9b8729-0c00-a162-6507-1798887712b0', N'Json', N'Json', NULL)
-
+--R022
 INSERT [dbo].[TpCUSmCsrTemplate] ([FId], [FName], [FIndex], [U_UpTemplate], [U_DownTemplate1], [U_DownTemplate2], [U_DownTemplate3], [FDescription], [U_ConnectionId], [U_DataModelUp], [U_DataModel], [FDataModel]) VALUES (N'c7bfb856-5000-8c56-0401-184e13b5e500', N'R022', 88, N'{
    "name":"R022",
    "from":"csr",
@@ -60,7 +63,7 @@ INSERT [dbo].[TpCUSmCsrTemplate] ([FId], [FName], [FIndex], [U_UpTemplate], [U_D
       "SVCSCRD": ""
    }
 }', N'', N'', N'', N'悠遊卡交易歷史檔查詢', N'ec9b8729-0c00-a162-6507-1798887712b0', N'Json', N'Json', NULL)
-
+--R031
 INSERT [dbo].[TpCUSmCsrTemplate] ([FId], [FName], [FIndex], [U_UpTemplate], [U_DownTemplate1], [U_DownTemplate2], [U_DownTemplate3], [FDescription], [U_ConnectionId], [U_DataModelUp], [U_DataModel], [FDataModel]) VALUES (N'c7bfb856-5000-8c56-0401-184e13ccec30', N'R031', 89, N'{
    "name":"R031",
    "from":"csr",
@@ -83,7 +86,7 @@ INSERT [dbo].[TpCUSmCsrTemplate] ([FId], [FName], [FIndex], [U_UpTemplate], [U_D
       "CORPIDN": ""
    }
 }', N'', N'', N'', N'信用卡特約商店資料主檔查詢', N'ec9b8729-0c00-a162-6507-1798887712b0', N'Json', N'Json', NULL)
-GO
+
 --R018
 INSERT [dbo].[TpCUSmCsrTemplate] ([FId], [FName], [FIndex], [U_UpTemplate], [U_DownTemplate1], [U_DownTemplate2], [U_DownTemplate3], [FDescription], [U_ConnectionId], [U_DataModelUp], [U_DataModel], [FDataModel]) VALUES (N'c7bfb856-5000-b864-bf02-185282bcc320', N'R018', 91, N'{
    "name":"R018",
@@ -94,7 +97,7 @@ INSERT [dbo].[TpCUSmCsrTemplate] ([FId], [FName], [FIndex], [U_UpTemplate], [U_D
       "CORPIDN": ""
    }
 }', N'', N'', N'', N'信用卡特約商店資料明細檔查詢', N'ec9b8729-0c00-a162-6507-1798887712b0', N'Json', N'Json', NULL)
-GO
+
 --R019
 INSERT [dbo].[TpCUSmCsrTemplate] ([FId], [FName], [FIndex], [U_UpTemplate], [U_DownTemplate1], [U_DownTemplate2], [U_DownTemplate3], [FDescription], [U_ConnectionId], [U_DataModelUp], [U_DataModel], [FDataModel]) VALUES (N'c7bfb856-5000-b864-bf02-185282dd74b0', N'R019', 92, N'{
    "name":"R019",
@@ -106,7 +109,7 @@ INSERT [dbo].[TpCUSmCsrTemplate] ([FId], [FName], [FIndex], [U_UpTemplate], [U_D
       "BRHNMBR":""
    }
 }', N'', N'', N'', N'本行信用卡特店主檔資料(分店)明細檔查詢', N'ec9b8729-0c00-a162-6507-1798887712b0', N'Json', N'Json', NULL)
-GO
+
 --R020
 INSERT [dbo].[TpCUSmCsrTemplate] ([FId], [FName], [FIndex], [U_UpTemplate], [U_DownTemplate1], [U_DownTemplate2], [U_DownTemplate3], [FDescription], [U_ConnectionId], [U_DataModelUp], [U_DataModel], [FDataModel]) VALUES (N'c7bfb856-5000-b864-bf02-1852835c3130', N'R020', 95, N'{
    "name":"R020",
@@ -117,7 +120,7 @@ INSERT [dbo].[TpCUSmCsrTemplate] ([FId], [FName], [FIndex], [U_UpTemplate], [U_D
       "CUSIDN": ""
    }
 }', N'', N'', N'', N'高風險客戶資料檔查詢', N'ec9b8729-0c00-a162-6507-1798887712b0', N'Json', N'Json', NULL)
-GO
+
 --R026
 INSERT [dbo].[TpCUSmCsrTemplate] ([FId], [FName], [FIndex], [U_UpTemplate], [U_DownTemplate1], [U_DownTemplate2], [U_DownTemplate3], [FDescription], [U_ConnectionId], [U_DataModelUp], [U_DataModel], [FDataModel]) VALUES (N'c7bfb856-5000-b864-bf02-185282ef7b70', N'R026', 93, N'{
    "name":"R026",
@@ -129,7 +132,7 @@ INSERT [dbo].[TpCUSmCsrTemplate] ([FId], [FName], [FIndex], [U_UpTemplate], [U_D
       "CRDID":""
    }
 }', N'', N'', N'', N'朝天宮點燈統計檔查詢', N'ec9b8729-0c00-a162-6507-1798887712b0', N'Json', N'Json', NULL)
-GO
+
 --R027
 INSERT [dbo].[TpCUSmCsrTemplate] ([FId], [FName], [FIndex], [U_UpTemplate], [U_DownTemplate1], [U_DownTemplate2], [U_DownTemplate3], [FDescription], [U_ConnectionId], [U_DataModelUp], [U_DataModel], [FDataModel]) VALUES (N'c7bfb856-5000-b864-bf02-185283888200', N'R027', 96, N'{
    "name":"R027",
@@ -141,7 +144,7 @@ INSERT [dbo].[TpCUSmCsrTemplate] ([FId], [FName], [FIndex], [U_UpTemplate], [U_D
       "KEYTYPE":""
    }
 }', N'', N'', N'', N'HCE資料檔查詢', N'ec9b8729-0c00-a162-6507-1798887712b0', N'Json', N'Json', NULL)
-GO
+
 
 --S005
 INSERT [dbo].[TpCUSmCsrTemplate] ([FId], [FName], [FIndex], [U_UpTemplate], [U_DownTemplate1], [U_DownTemplate2], [U_DownTemplate3], [FDescription], [U_ConnectionId], [U_DataModelUp], [U_DataModel], [FDataModel]) VALUES (N'c7bfb856-5000-b864-bf02-1852843259a0', N'S005', 97, N'{
@@ -153,7 +156,7 @@ INSERT [dbo].[TpCUSmCsrTemplate] ([FId], [FName], [FIndex], [U_UpTemplate], [U_D
       "CUSIDN": ""
    }
 }', N'', N'', N'', N'客戶身份證偽造或遺失記錄檔查詢', N'ec9b8729-0c00-a162-6507-1798887712b0', N'Json', N'Json', NULL)
-GO
+
 
 --R032
 INSERT [dbo].[TpCUSmCsrTemplate] ([FId], [FName], [FIndex], [U_UpTemplate], [U_DownTemplate1], [U_DownTemplate2], [U_DownTemplate3], [FDescription], [U_ConnectionId], [U_DataModelUp], [U_DataModel], [FDataModel]) VALUES (N'c7bfb856-5000-b864-bf02-1852831ec540', N'R032', 94, N'{
@@ -165,6 +168,6 @@ INSERT [dbo].[TpCUSmCsrTemplate] ([FId], [FName], [FIndex], [U_UpTemplate], [U_D
       "CARDNUM": ""
    }
 }', N'', N'', N'', N'依卡號查詢扣款帳號', N'ec9b8729-0c00-a162-6507-1798887712b0', N'Json', N'Json', NULL)
-GO
+
 
 --/電文------------------------------------------------------------------------------------------------------
